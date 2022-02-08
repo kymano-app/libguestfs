df1a16123130704b79f324fae1558c111ac70c7e
Alex-Braine <alex.braine.lu@gmail.com>
2022-02-08 19:22:37 +0100

    upd

--- libguestfs-win.sh
+++ libguestfs-win.sh
@@ -56,6 +56,6 @@ patch common/utils/pcre2-cleanups.c ../libguestfs-patches-1.44.2-2-win/common/ut
 
 autoreconf -i
 
-./configure --with-qemu="no" --enable-debug --disable-appliance --enable-vala=no --disable-rust --enable-introspection=no --disable-gobject --disable-golang --disable-lua --disable-php --disable-erlang --disable-haskell --disable-ruby --disable-python  --disable-perl --disable-ocaml --disable-libtool-lock --disable-dependency-tracking --disable-option-checking --disable-rust --with-distro=openbsd --disable-probes  --disable-daemon --disable-fuse LDFLAGS="-L/opt/local/lib -L/usr/local/lib" LIBS="-lintl"
+./configure --with-qemu="no" --enable-debug --disable-appliance --enable-vala=no --disable-rust --enable-introspection=no --disable-gobject --disable-golang --disable-lua --disable-php --disable-erlang --disable-haskell --disable-ruby --disable-python  --disable-perl --disable-ocaml --disable-libtool-lock --disable-dependency-tracking --disable-option-checking --disable-rust --with-distro=openbsd --disable-probes  --disable-daemon --disable-fuse LDFLAGS="-L/usr/local/lib -L/usr/lib" LIBS="-lintl"
 
 make -t
