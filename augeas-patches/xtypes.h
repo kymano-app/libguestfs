7fbcc501351dc85652ec1e8a7f18f6f70b861a32
Alex-Braine <alex.braine.lu@gmail.com>
2022-02-13 20:48:41 +0100

    upd

---
+++ augeas-patches/xtypes.h
@@ -0,0 +1,3 @@
+typedef unsigned short  ushort;         /* System V compatibility */
+typedef unsigned int    uint;           /* System V compatibility */
+typedef unsigned long   ulong;          /* System V compatibility */
